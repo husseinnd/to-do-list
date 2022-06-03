a4b794ebdb08e85e94fef452feaf248f90527214
husseinND <57720842+husseinnd@users.noreply.github.com>
2022-06-03 21:56:09 +0300

    implementation of update function api

--- src/views/to-do-list.js
+++ src/views/to-do-list.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer, useRef } from "react";
 import { useAuth } from "../api/auth";
 import { useToDo } from "../api/todo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,13 +15,24 @@ function ToDoList() {
   const navigate = useNavigate();
   const auth = useAuth();
   const todo = useToDo();
-  const [taskInput, setTaskInput] = useState("");
+  const taskInputRefs = useRef([]);
+  const [addTaskInput, setAddTaskInput] = useState("");
+  const [editMode, setEditMode] = useReducer(
+    (editMode, newEditMode) => ({ ...editMode, ...newEditMode }),
+    {
+      isEdit: false,
+      id: "",
+      text: "",
+    }
+  );
 
+  //getting all the data for the tasks
   useEffect(() => {
     document.title = "To Do List";
     todo.getAll();
   }, []);
 
+  //logout function
   const logout = (e) => {
     // sure valid so submit
     auth.logout().then(() => {
@@ -29,21 +40,66 @@ function ToDoList() {
     });
   };
 
+  const editTask = (e, description) => {
+    if (e.key === "Enter") {
+      if (editMode.text === description) {
+        resetEditMode();
+        return;
+      }
+      if (!editMode.text) {
+        alert("Please Enter a Task Description");
+        return;
+      }
+      todo.update(editMode.id, { description: editMode.text });
+      resetEditMode();
+    }
+  };
+
+  //this function is to reset the data in the edit mode state after exiting edit mode.
+  const resetEditMode = () => {
+    setEditMode({
+      isEdit: false,
+      id: "",
+      text: "",
+    });
+  };
+
+  //this is to render the to do list tasks.
   const renderTasks = () => {
     return (
       <div className="tasks-container">
-        {todo.list.map(({ completed, description }, i) => {
+        {todo.list.map(({ completed, description, _id: id }, i) => {
+          const isEditMode = editMode.id === id;
           return (
-            <div key={`to-do-list-item-${i}`} className="task-row">
+            <div
+              key={`to-do-list-item-${i}`}
+              className={`task-row ${isEditMode ? "edit-mode" : ""}`}
+            >
               <span
                 className={`completed-status ${completed ? "completed" : ""}`}
               >
                 <FontAwesomeIcon icon={!completed ? faCircle : faCircleCheck} />
               </span>
-              <span className="task-desc">{description}</span>
+              <input
+                type="text"
+                className="task-desc"
+                value={isEditMode ? editMode.text : description}
+                ref={(el) => (taskInputRefs.current[i] = el)}
+                onKeyDown={(e) => editTask(e, description)}
+                onChange={(e) => setEditMode({ text: e.target.value })}
+                disabled={isEditMode ? !editMode.isEdit : true}
+              />
               <div className="task-actions">
                 <span>
-                  <FontAwesomeIcon icon={faPencil} />
+                  <FontAwesomeIcon
+                    icon={faPencil}
+                    onClick={(e) => {
+                      setEditMode({ id, isEdit: true, text: description });
+                      setTimeout(() => {
+                        taskInputRefs.current[i].focus();
+                      }, 0);
+                    }}
+                  />
                 </span>
                 <span>
                   <FontAwesomeIcon icon={faTrash} />
@@ -56,21 +112,24 @@ function ToDoList() {
     );
   };
 
+  //add task on keyboard enter key press
   const addOnEnter = (e) => {
     if (e.key === "Enter") {
-      todo.add(taskInput);
+      todo.add({ description: addTaskInput });
     }
   };
 
+  //this is to render the input field for user input
   const renderCreateTask = () => {
     return (
-      <div>
+      <div className="add-task">
         <FontAwesomeIcon icon={faPlusCircle} />
         <input
           type="text"
-          value={taskInput}
-          onChange={(e) => setTaskInput(e.target.value)}
+          value={addTaskInput}
+          onChange={(e) => setAddTaskInput(e.target.value)}
           onKeyDown={addOnEnter}
+          placeholder="Add Task"
         />
       </div>
     );
@@ -79,8 +138,10 @@ function ToDoList() {
   return (
     <div className="to-do-list">
       <div className="to-do-list-container">
-        <h2 className="title">To do list</h2>
-        <span onClick={logout}>Logout</span>
+        <div className="container-header">
+          <h2 className="title">To do list</h2>
+          <button onClick={logout}>Logout</button>
+        </div>
         <div>
           {renderCreateTask()}
           {renderTasks()}
